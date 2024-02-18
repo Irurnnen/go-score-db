0c86c8ccb2eb464e138870b83da19e47e8b75be1
Irurnnen <i19v199a56n78@gmail.com>
2024-02-18 20:07:04 +0400

    Hotfix
    	modified:   .gitignore
    	modified:   README.md
    	modified:   postgres/init/create_structure.sql

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 ### Переход между репозиториями проетка:
 
-- [go-score-gui](https://example.com)
-- [go-score-orchestra](https://example.com)
-- [go-score-db](https://example.com)
-- [go-score-solver](https://example.com)
+- [go-score-gui](https://github.com/Irurnnen/go-score-gui)
+- [go-score-orchestra](https://github.com/Irurnnen/go-score-orchestra)
+- Вы здесь:[go-score-db](https://github.com/Irurnnen/go-score-db)
+- [go-score-solver](https://github.com/Irurnnen/go-score-solver)
 
 ### Запуск:
 
@@ -18,5 +18,10 @@
     PORT=6875
     ```
 
-2. Запустить docker командой `docker compose up --detach`
+2. Запустить docker контейнер командой `docker compose up --detach`
 
+### Остановка:
+
+1. Остановить docker контейнер с помощью compose: команда `docker compose down `
+
+2. Остановить docker контейнер: команда `docker stop lpz-db` 
--- postgres/init/create_structure.sql
+++ postgres/init/create_structure.sql
@@ -1,5 +1,6 @@
 CREATE TYPE actions AS ENUM ('+', '-', '*', '/');
-CREATE TYPE statuses AS ENUM ('created', 'assigned', 'resolved', 'unresolved')
+
+CREATE TYPE statuses AS ENUM ('created', 'assigned', 'resolved', 'unresolved');
 
 CREATE TABLE IF NOT EXISTS Expressions (
     expression_id SERIAL PRIMARY KEY,
@@ -9,15 +10,15 @@ CREATE TABLE IF NOT EXISTS Expressions (
     solved_time INTEGER NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS expression_and_task (
-    expression_id INTEGER REFERENCES Expressions(expression_id) NOT NULL,
-    task_id INTEGER REFERENCES Tasks(task_id) NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS Tasks (
     task_id SERIAL PRIMARY KEY,
     solved BOOLEAN,
     num_A INTEGER NOT NULL,
     num_B INTEGER NOT NULL,
     action_type actions NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS expression_and_task (
+    expression_id INTEGER REFERENCES Expressions(expression_id) NOT NULL,
+    task_id INTEGER REFERENCES Tasks(task_id) NOT NULL
 );
